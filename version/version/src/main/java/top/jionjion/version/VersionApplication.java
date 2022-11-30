c16e9f96e5650395953824dcfedb421d7cd0adf3
jionjion <jionjion@aliyun.com>
2022-11-30 21:12:16 +0800

    2022-11-30

---
+++ version/version/src/main/java/top/jionjion/version/VersionApplication.java
@@ -0,0 +1,18 @@
+package top.jionjion.version;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 主版本信息
+ *
+ * @author Jion
+ */
+@SpringBootApplication
+public class VersionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VersionApplication.class, args);
+    }
+
+}
