5d1fc948cf8d89f0a9d52a2adf86b99c63913a6f
jionjion <jionjion@aliyun.com>
2022-12-04 10:42:54 +0800

    2022-12-04

---
+++ util-starter/src/main/java/top/jionjion/util/weather/WeatherAutoConfiguration.java
@@ -0,0 +1,36 @@
+package top.jionjion.util.weather;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 自动配置类.
+ * 指定配置信息类, 与自动注入的条件
+ *
+ * @author Jion
+ */
+@Configuration
+@EnableConfigurationProperties(WeatherProperty.class)
+@ConditionalOnProperty(name = "weather.enable", havingValue = "true")
+public class WeatherAutoConfiguration {
+
+    private final WeatherProperty weatherProperty;
+
+    @Autowired
+    public WeatherAutoConfiguration(WeatherProperty weatherProperty) {
+        this.weatherProperty = weatherProperty;
+    }
+
+    /**
+     * 没有自定义服务时, 自动创建
+     */
+    @Bean
+    @ConditionalOnMissingBean(WeatherService.class)
+    public WeatherService weatherSource() {
+        return new WeatherService(weatherProperty);
+    }
+}
---
+++ util-starter/src/test/java/top/jionjion/util/UtilStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package top.jionjion.util;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class UtilStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertDoesNotThrow(() -> UtilStarterApplication.main(new String[]{""}));
+    }
+
+}
