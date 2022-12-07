436982feec8bb75becd40c69d902c460a011e561
jionjion <jionjion@aliyun.com>
2022-12-07 22:16:37 +0800

    2022-12-07

--- core-web/src/test/java/top/jionjion/core/starter/UseWeatherServiceTest.java
+++ core-web/src/test/java/top/jionjion/core/starter/UseWeatherServiceTest.java
@@ -1,7 +1,5 @@
 package top.jionjion.core.starter;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
@@ -10,12 +8,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UseWeatherServiceTest {
 
-    @Autowired
-    UseWeatherService useWeatherService;
-
-    @Test
-    void getType(){
-        String type = useWeatherService.getType();
-        System.out.println("天气状态>> " + type);
-    }
+//    @Autowired
+//    UseWeatherService useWeatherService;
+//
+//    @Test
+//    void getType(){
+//        String type = useWeatherService.getType();
+//        System.out.println("天气状态>> " + type);
+//    }
 }
