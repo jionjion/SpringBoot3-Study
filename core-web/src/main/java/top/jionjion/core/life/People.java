4506aec17aec8261e7b8131e1ce762b96084c9d7
jionjion <jionjion@aliyun.com>
2023-01-12 11:02:58 +0800

    2023-01-12

--- core-web/src/main/java/top/jionjion/core/life/People.java
+++ core-web/src/main/java/top/jionjion/core/life/People.java
@@ -1,6 +1,10 @@
 package top.jionjion.core.life;
 
+import jakarta.annotation.PostConstruct;
 import lombok.Data;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 /**
@@ -11,7 +15,42 @@ import org.springframework.stereotype.Component;
 @Data
 @Component
 @SuppressWarnings("unused")
-public class People {
+public class People implements InitializingBean, DisposableBean {
 
     private String name;
+
+    @Value("${spring.datasource.url}")
+    String datasourceUrl;
+
+    /**
+     * 优先执行,初始化方法
+     * 已经 new 创建对象后执行, 尚未对 new 出的对象进行过属性赋值..
+     *
+     * @throws Exception 如果发生了异常
+     */
+    @PostConstruct
+    public void init() throws Exception {
+        System.out.println("People 初始化, 通过 @PostConstruct 注解" + this);
+    }
+
+    /**
+     * 其次执行, 初始化方法
+     * 此时, Bean 通过getBean() 方法已经创建过后, 尚未对 new 出的对象进行过属性赋值..
+     *
+     * @throws Exception 如果发生了异常
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("People 初始化, 通过 InitializingBean 接口: " + this);
+    }
+
+    /**
+     * 销毁方法, 容器关闭时执行
+     *
+     * @throws Exception 如果发生了异常
+     */
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("People 已经, 通过 DisposableBean 接口: " + this);
+    }
 }
