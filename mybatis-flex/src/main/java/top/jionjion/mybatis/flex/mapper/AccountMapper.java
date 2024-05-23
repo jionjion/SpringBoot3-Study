3cd96eea11f320d5f4213d711ca91a21fa774393
jionjion <jionjion@aliyun.com>
2024-05-23 19:16:52 +0800

    试用 mybatis-flex

--- mybatis-flex/src/main/java/top/jionjion/mybatis/flex/mapper/AccountMapper.java
+++ mybatis-flex/src/main/java/top/jionjion/mybatis/flex/mapper/AccountMapper.java
@@ -1,6 +1,8 @@
 package top.jionjion.mybatis.flex.mapper;
 
 import com.mybatisflex.core.BaseMapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import top.jionjion.mybatis.flex.entity.Account;
 
 /**
@@ -10,4 +12,20 @@ import top.jionjion.mybatis.flex.entity.Account;
  */
 public interface AccountMapper extends BaseMapper<Account> {
 
+    /**
+     * 自定义查询, 注解驱动
+     *
+     * @param id 主键
+     * @return 查询结果
+     */
+    @Select("select * from tb_account where id = #{id,jdbcType=BIGINT}")
+    Account selectById(@Param("id") Long id);
+
+    /**
+     * 试用 xlm 查询
+     *
+     * @param userName 用户名
+     * @return 查询结果
+     */
+    Account selectByUserName(@Param("userName") String userName);
 }
--- mybatis-flex/src/test/java/top/jionjion/mybatis/flex/mapper/AccountMapperTest.java
+++ mybatis-flex/src/test/java/top/jionjion/mybatis/flex/mapper/AccountMapperTest.java
@@ -1,12 +1,16 @@
 package top.jionjion.mybatis.flex.mapper;
 
+import com.mybatisflex.core.paginate.Page;
 import com.mybatisflex.core.query.QueryWrapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import top.jionjion.mybatis.flex.entity.Account;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+
+import static top.jionjion.mybatis.flex.entity.table.AccountTableDef.ACCOUNT;
 
 /**
  * 测试账号
@@ -20,10 +24,50 @@ class AccountMapperTest {
     private AccountMapper accountMapper;
 
 
+    /**
+     * 测试框架
+     */
     @Test
     void test() {
-        QueryWrapper queryWrapper = QueryWrapper.create().select();
-        List<Account> accountList = accountMapper.selectListByQuery(queryWrapper);
-        System.out.println(accountList);
+        QueryWrapper queryWrapper = QueryWrapper.create().select().where(ACCOUNT.ID.eq(2));
+        Account account = accountMapper.selectOneByQuery(queryWrapper);
+        System.out.println(account);
+    }
+
+    /**
+     * 测试注解驱动的sql
+     */
+    @Test
+    void selectById() {
+        Account account = accountMapper.selectById(1L);
+        System.out.println(account);
+    }
+
+    /**
+     * 测试xml的sql
+     */
+    @Test
+    void selectByUserName() {
+        Account account = accountMapper.selectByUserName("张三");
+        System.out.println(account);
+    }
+
+    /**
+     * 测试xml的sql,并使用分页效果
+     */
+    @Test
+    void selectByUserNameWithPage() {
+        Account account = accountMapper.selectByUserName("张三");
+        QueryWrapper queryWrapper = QueryWrapper.create()
+                .where(Account::getAge).eq(18)
+                .and(Account::getId).ge(0);
+
+        // 可选其他参数
+        Map<String, Object> otherParams = new HashMap<>();
+        otherParams.put("userName", "张三");
+
+        Page<Account> accountPage = accountMapper.xmlPaginate("top.jionjion.mybatis.flex.mapper.AccountMapper.selectByUserName",
+                Page.of(1, 10), queryWrapper, otherParams);
+        System.out.println(accountPage);
     }
 }
