59df4dea167191ac23d68394c249eb43a76d35f1
jionjion <jionjion@aliyun.com>
2024-05-27 21:10:22 +0800

    试用 mybatis-flex

--- mybatis-flex/src/main/java/top/jionjion/mybatis/flex/Application.java
+++ mybatis-flex/src/main/java/top/jionjion/mybatis/flex/Application.java
@@ -4,11 +4,16 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * mybatis-flex 试用
+ *
+ * @author Jion
+ */
 @MapperScan("top.jionjion.mybatis.flex.mapper")
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
--- mybatis-flex/src/test/java/top/jionjion/mybatis/flex/mapper/AccountMapperBaseUpdateTest.java
+++ mybatis-flex/src/test/java/top/jionjion/mybatis/flex/mapper/AccountMapperBaseUpdateTest.java
@@ -1,6 +1,9 @@
 package top.jionjion.mybatis.flex.mapper;
 
 import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.core.update.UpdateChain;
+import com.mybatisflex.core.update.UpdateWrapper;
+import com.mybatisflex.core.util.UpdateEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -121,5 +124,61 @@ class AccountMapperBaseUpdateTest {
         account.setUserName("张四");
         QueryWrapper queryWrapper = QueryWrapper.create().where(AccountTableDef.ACCOUNT.ID.eq(1));
         accountMapper.updateByQuery(account, false, queryWrapper);
+    }
+
+
+    /**
+     * 只更新部分字段
+     */
+    @Test
+    void updateWithPartColumn() {
+        //Account account = UpdateEntity.of(Account.class);
+        //account.setId(1L);
+        // 等价写法
+        Account account = UpdateEntity.of(Account.class, 1);
+
+        // 更新名字为空, 年龄为10
+        account.setUserName(null);
+        account.setAge(10);
+
+        accountMapper.update(account);
+    }
+
+    /**
+     * 更新部分字段,并使用数据库函数
+     */
+    @Test
+    void updateWithPartColumnAndFunction() {
+        Account account = UpdateEntity.of(Account.class);
+        account.setId(1L);
+
+        // 姓名更新为空
+        account.setUserName(null);
+
+        // 通过 UpdateWrapper 操作 account 数据, 增强更新操作
+        UpdateWrapper<Account> wrapper = UpdateWrapper.of(account);
+        // 使用数据库函数 set age = age+1
+        wrapper.setRaw("age", "age + 1");
+        // set age = age + 1
+        wrapper.setRaw(AccountTableDef.ACCOUNT.AGE, AccountTableDef.ACCOUNT.AGE.add(1));
+        // set age = (select max(age) from account)
+        wrapper.setRaw(AccountTableDef.ACCOUNT.AGE, "(select max(age) from account)");
+        // 执行更新
+        accountMapper.update(account);
+    }
+
+    /**
+     * 使用更新链式操作
+     */
+    @Test
+    void updateChain() {
+        UpdateChain.of(Account.class)
+                // set 指定字段
+                .set(Account::getUserName, "张三")
+                // setRaw 指定字段并参与 SQL 拼接, 有注入的风险
+                .setRaw(Account::getAge, "age + 1")
+                .setRaw(Account::getBirthday, "(select max(birthday) from account)")
+                .where(Account::getId).eq(1)
+                .update();
     }
 }
